c133ef7a42aea79f0eddc91d424a13428dc74107
Muhammad Anas Raza <63569834+anas-rz@users.noreply.github.com>
2024-08-26 12:54:39 -0400

    Create model.py

---
+++ locator_graph/fault_injector/model.py
@@ -0,0 +1,94 @@
+# from models.TRANSFORMER1 import TRANSFORMER1
+# from models.TRANSFORMER2 import TRANSFORMER2
+# from models.TRANSFORMER3 import TRANSFORMER3
+# from models.TRANSFORMER4 import TRANSFORMER4
+# from models.TRANSFORMER5 import TRANSFORMER5
+# from models.TRANSFORMER6 import TRANSFORMER6
+# from models.TRANSFORMER7 import TRANSFORMER7
+# from models.TRANSFORMER8 import TRANSFORMER8
+# from models.TRANSFORMER9 import TRANSFORMER9
+# from models.TRANSFORMER10 import TRANSFORMER10
+# from models.TRANSFORMER11 import TRANSFORMER11
+# from models.TRANSFORMER12 import TRANSFORMER12
+# from models.TRANSFORMER13 import TRANSFORMER13
+# from models.TRANSFORMER14 import TRANSFORMER14
+# from models.TRANSFORMER15 import TRANSFORMER15
+
+
+from models.LSTM1 import LSTM1
+from models.LSTM2 import LSTM2
+from models.LSTM3 import LSTM3
+from models.LSTM4 import LSTM4
+from models.LSTM5 import LSTM5 ####
+from models.LSTM6 import LSTM6
+from models.LSTM7 import LSTM7
+from models.LSTM8 import LSTM8_9 as LSTM8
+from models.LSTM9 import LSTM8_9 as LSTM9
+from models.LSTM10 import LSTM10
+
+
+# from models.GRU1 import GRU1
+# from models.GRU2 import GRU2
+from models.GRU3 import GRU3
+from models.GRU4 import GRU4
+# from models.GRU5 import GRU5 ####
+# from models.GRU6 import GRU6
+from models.GRU7 import GRU7
+# from models.GRU8 import GRU8
+# from models.GRU9 import GRU9
+# from models.GRU10 import GRU10
+
+
+# from models.RNN1 import RNN1
+# from models.RNN2 import RNN2
+# from models.RNN3 import RNN3
+# from models.RNN4 import RNN4
+# from models.RNN5 import RNN5
+# from models.RNN6 import RNN6
+# from models.RNN7 import RNN7
+# from models.RNN8 import RNN8
+# from models.RNN9 import RNN9
+# from models.RNN10 import RN10
+
+# from models.MISC1 import MISC1
+# from models.MISC2 import MISC2
+# from models.MISC3 import MISC3
+MODEL_TYPES = {
+# 'TRANSFORMER1': TRANSFORMER1,
+# 'TRANSFORMER2': TRANSFORMER2,
+# 'TRANSFORMER3': TRANSFORMER3,
+# 'TRANSFORMER4': TRANSFORMER4,
+# 'TRANSFORMER5': TRANSFORMER5,
+# 'TRANSFORMER6': TRANSFORMER6,
+# 'TRANSFORMER8': TRANSFORMER8,
+# 'TRANSFORMER9': TRANSFORMER9,
+# 'TRANSFORMER10': TRANSFORMER10,
+# 'TRANSFORMER11': TRANSFORMER11,
+# 'TRANSFORMER11': TRANSFORMER12,
+# 'TRANSFORMER13': TRANSFORMER13,
+# 'TRANSFORMER14': TRANSFORMER14,
+# 'TRANSFORMER15': TRANSFORMER15,
+                "LSTM1": LSTM1,
+ "LSTM2": LSTM2,
+#  "LSTM3": LSTM3,
+ "LSTM4": LSTM4,
+ "LSTM5": LSTM5,
+ "LSTM6": LSTM6,
+ "LSTM7": LSTM7,
+ "LSTM8": LSTM8,
+ "LSTM9": LSTM9,
+# "LSTM10": LSTM10,
+
+
+#  "GRU1": GRU1,
+#  "GRU2": GRU2,
+ "GRU3": GRU3,
+ "GRU4": GRU4,
+#  "GRU5": GRU5,
+#  "GRU6": GRU6,
+ "GRU7": GRU7,
+#  "GRU8": GRU8,
+#  "GRU9": GRU9,
+# "GRU10": GRU10,
+
+                }
